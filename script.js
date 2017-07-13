a312f50288cd526cd1b975c44eca1478b0336f15
Joshua Harshaw <joshuaharshaw@gmail.com>
2017-07-13 15:40:02 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,12 @@
+var fahrenheit = 77;
+
+function toCelcius (temp) {
+
+var answer = fahrenheit + " degrees Fahrenheit converts to " + (((temp - 32) * 5) / 9) + " degrees Celcius.";
+console.log(answer);
+
+}
+
+toCelcius(fahrenheit);
+
+console.log('Code by Joshua Harshaw.');
